c1d5a86f4cd78ab071bc34d771bd8e49c280c3d5
Maung Maung Kha <maungmaungkha18290@gmail.com>
2023-02-24 14:39:39 +0100

    cloudinary test

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,7 @@ class BookmarksController < ApplicationController
 
 
   def new
-    @list= List.find(params[:list_id])
+    @list = List.find(params[:list_id])
 
     @bookmark = Bookmark.new
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # cloudinary service
+  config.active_storage.service = :cloudinary
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
